a080def70296a819c0d6c4e9134709d3d40c6c02
Lucifer <166632710+thuangf45@users.noreply.github.com>
2026-04-23 19:44:54 +0700

    Create RevenueReport.cs

---
+++ src/Server/Handler/Report/Data/RevenueReport.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Handler.Report.Data;
+
+public class RevenueReport
+{
+    public string Time { get; set; } = null!;
+    public decimal Revenue { get; set; }
+    public decimal Profit { get; set; }
+}
