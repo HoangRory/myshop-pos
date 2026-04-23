b083760fe3f4d0913ee478f6aebbc4ff4d0f4ac9
Lucifer <166632710+thuangf45@users.noreply.github.com>
2026-04-23 19:48:32 +0700

    Update ReportHandler.cs

--- src/Server/Handler/Report/ReportHandler.cs
+++ src/Server/Handler/Report/ReportHandler.cs
@@ -1,4 +1,5 @@
 ﻿using LuciferCore.Attributes;
+using LuciferCore.Extensions;
 using LuciferCore.Handler;
 using LuciferCore.Model;
 using LuciferCore.Service;
@@ -9,17 +10,33 @@ namespace Server.Handler.Report;
 [Handler("v1", "/api/report")]
 public class ReportHandler : RouteHandler
 {
+    private readonly ReportService _reportService = new();
+
     [RateLimiter(100, 60)]
+#if DEBUG
+    [Authorize(UserRole.Guest)]
+#else 
     [Authorize(UserRole.User)]
+#endif
     [HttpPost("/product")]
-    private void GetProductReport([Session] AppSession session, [Data] RequestModel request)
+    private async Task GetProductReport([Session] AppSession session, [Data] RequestModel request)
     {
+        var filter = request.BodySpan.FromJson<ReportFilter>();
+        using var response = await _reportService.GetProductReport(filter);
+        session.SendResponseAsync(response);
     }
 
     [RateLimiter(100, 60)]
+#if DEBUG
+    [Authorize(UserRole.Guest)]
+#else 
     [Authorize(UserRole.User)]
+#endif
     [HttpPost("/revenue")]
-    private void GetRevenueReport([Session] AppSession session, [Data] RequestModel request)
+    private async Task GetRevenueReport([Session] AppSession session, [Data] RequestModel request)
     {
+        var filter = request.BodySpan.FromJson<ReportFilter>();
+        using var response = await _reportService.GetRevenueReport(filter);
+        session.SendResponseAsync(response);
     }
 }
