3470949d6ade57ae49845ae89842b0dbda8aeb8a
Lucifer <166632710+thuangf45@users.noreply.github.com>
2026-05-08 22:37:31 +0700

    Add BackupRestore model
    Introduce BackupRestore class in MyShop.Client.Models to represent backup metadata. It includes Name, CreateAt (DateTime) and Size with a default value of "0 KB" to track backup file properties on the client.

---
+++ src/Client/MyShop.Client/Models/BackupRestore.cs
@@ -0,0 +1,9 @@
+﻿namespace MyShop.Client.Models
+{
+    public class BackupRestore
+    {
+        public string Name { get; set; }
+        public DateTime CreateAt { get; set; }
+        public string Size { get; set; } = "0 KB";
+    }
+}
