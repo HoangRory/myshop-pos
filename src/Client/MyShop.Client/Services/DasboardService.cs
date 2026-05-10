b32c1e72dc074f1c0fa2486ec3d568f84e961f8b
Lucifer <166632710+thuangf45@users.noreply.github.com>
2026-05-11 00:19:30 +0700

    Create DasboardService.cs

---
+++ src/Client/MyShop.Client/Services/DasboardService.cs
@@ -0,0 +1,23 @@
+﻿using LuciferCore.Attributes;
+using MyShop.Client.Models;
+using MyShop.Client.Services.Interfaces;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace MyShop.Client.Services
+{
+    [Plugin("Service", "Dashboard")]
+    public class DashboardService : IDashboardService
+    {
+        private readonly HttpClient _httpClient;
+        private const string ApiPath = "/v1/api/dashboard";
+        private string Url(string endpoint = "") => ((IAPI)this).GetFullUrl(ApiPath, endpoint);
+        public DashboardService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<Dashboard> GetDashboardDataAsync()
+            => await _httpClient.GetFromJsonAsync<Dashboard>(Url()) ?? new();
+    }
+}
